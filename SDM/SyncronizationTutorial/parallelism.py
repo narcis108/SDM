05d925ac226922c17d23c60e84476d370b382a76
Zbarcea Narcis <zbarcea.narcis@gmail.com>
2017-11-14 12:59:44 +0200

    versiune finala

--- SDM/SyncronizationTutorial/parallelism.py
+++ SDM/SyncronizationTutorial/parallelism.py
@@ -16,34 +16,38 @@ def build_directory_list(folder):
     return list
 
 
-files = build_directory_list("D:\EclipseWorkSpace")
+files = build_directory_list("H:\pycharm-edu-2.0.3")
 
 
 def makePool(num_threads):
     # Make the Pool of workers
     pool = ThreadPool(num_threads)
-    results = pool.map(logging.debug, files)
+    results = pool.map(print, files)
     pool.close()
     pool.join()
 
 
 def show_results(func_type, best_result):
-    print("%s %f seconds" % (func_type, best_result))
+    result = "%s %f seconds" % (func_type, best_result)
+    return result
 
 
-def sequential_run(files):
+def secvential(files):
     for file in files:
         print(file)
+
 
 if __name__ == "__main__":
     repeat = 1
     number = 1
-    num_threads = [2]
+    num_threads = [8]
     for i in num_threads:
         t = Timer("makePool(%s)" % i, "from __main__ import makePool")
         best_result = min(t.repeat(repeat=repeat, number=number))
-        show_results("thredead(%s threads)" % i, best_result)
+        result_thredead = show_results("thredead(%s threads)" % i, best_result)
 
-    t = Timer("sequential_run", "from __main__ import sequential_run")
+    t = Timer("secvential(%s)" % files, "from __main__ import secvential")
     best_result = min(t.repeat(repeat=repeat, number=number))
-    show_results("sequential() : ", best_result)
+    print('\n')
+    print(show_results("secvential() : ", best_result))
+    print(result_thredead)
